13c1f7550f3f29a7bf7a08d79cd99c84c79c4927
Julian Maunder <julianmattmaunder@gmail.com>
2014-11-18 10:17:50 -0800

    tweaks

--- scripts.js
+++ scripts.js
@@ -1,22 +1,28 @@
 $( document ).ready(function() {
 	$('body').each(function(){
-		$(window).scroll(function() {
-			// Store scrollTop in variable
-			var scrollPos = $(window).scrollTop();
-			// var viewportHeight = $(window).height();
-			console.log(scrollPos);
-			var ballPos = scrollPos - 500;
-			var bouncePos = ((450 - ballPos) * 3) + ballPos;
-			console.log(ballPos + 'ball');
-			// .css({ 'transform': 'translate3d(0px, ' + ballPos + 'px, 0) scale('+ scale +')'});
-			$(".ball").css({ 'transform': 'translate3d(0px, ' + ballPos + 'px, 0)'});
-			$(".presents").css({ 'transform': 'translate3d(0px, -' + scrollPos * 2 + 'px, 0)'});
-			if (scrollPos > 950) {
-			$(".ball").css({ 'transform': 'translate3d(0px, ' + bouncePos + 'px, 0)'});
-			}
-			// else {
-			// 	$(".ball").css({transform: 'translateY(0)'})
-			// };
-		});
+		
+		function headerParallax() {
+			$(window).scroll(function() {
+				// Store scrollTop in variable
+				var scrollPos = $(window).scrollTop();
+				// var viewportHeight = $(window).height();
+				// console.log(scrollPos);
+				// console.log(ballPos + 'ball');
+
+				var ballPos = scrollPos - 500;
+				var bouncePos = (((650 - ballPos) * 3) + ballPos).toFixed(2);
+				
+				$(".ball").css({ 'transform': 'translate3d(0px, ' + ballPos + 'px, 0)'});
+				$(".presents").css({ 'transform': 'translate3d(0px, -' + scrollPos * 2 + 'px, 0)'});
+				
+				if (scrollPos > 1150) {
+					$(".ball").css({ 'transform': 'translate3d(0px, ' + bouncePos + 'px, 0)'});
+				}
+			
+			});
+		}
+
+		window.requestAnimationFrame(headerParallax);
+
 	});
 });
