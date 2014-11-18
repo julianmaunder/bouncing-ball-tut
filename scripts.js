54834c55fad48e38ff12cac512ea9a915e08469c
Julian Maunder <julianmattmaunder@gmail.com>
2014-11-18 12:19:26 -0800

    tweaks, new lesson name

--- scripts.js
+++ scripts.js
@@ -6,16 +6,16 @@ $( document ).ready(function() {
 				// Store scrollTop in variable
 				var scrollPos = $(window).scrollTop();
 				// var viewportHeight = $(window).height();
-				// console.log(scrollPos);
-				// console.log(ballPos + 'ball');
+				console.log(scrollPos);
+				console.log(ballPos + 'ball');
 
 				var ballPos = scrollPos - 500;
-				var bouncePos = (((650 - ballPos) * 3) + ballPos).toFixed(2);
+				var bouncePos = (((490 - ballPos) * 2.5) + ballPos).toFixed(2);
 				
 				$(".ball").css({ 'transform': 'translate3d(0px, ' + ballPos + 'px, 0)'});
 				$(".presents").css({ 'transform': 'translate3d(0px, -' + scrollPos * 2 + 'px, 0)'});
 				
-				if (scrollPos > 1150) {
+				if (scrollPos > 990) {
 					$(".ball").css({ 'transform': 'translate3d(0px, ' + bouncePos + 'px, 0)'});
 				}
 			
