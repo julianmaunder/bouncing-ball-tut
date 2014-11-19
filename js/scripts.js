a3011051adc974f00d9ec0a525986357cc0b670d
Julian Maunder <julianmattmaunder@gmail.com>
2014-11-19 04:12:46 -0800

    added new fonts and bootstrap grid only

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+	$('body').each(function(){
+		
+		function headerParallax() {
+			$(window).scroll(function() {
+				// Store scrollTop in variable
+				var scrollPos = $(window).scrollTop();
+				// var viewportHeight = $(window).height();
+				console.log(scrollPos);
+				console.log(ballPos + 'ball');
+
+				var ballPos = scrollPos - 500;
+				var bouncePos = (((550 - ballPos) * 2.5) + ballPos).toFixed(2);
+				
+				$(".ball").css({ 'transform': 'translate3d(0px, ' + ballPos + 'px, 0)'});
+				$(".presents").css({ 'transform': 'translate3d(0px, -' + scrollPos * 2 + 'px, 0)'});
+				
+				if (scrollPos > 1050) {
+					$(".ball").css({ 'transform': 'translate3d(0px, ' + bouncePos + 'px, 0)'});
+				}
+
+				if (scrollPos > 1040 && scrollPos > 1055) {
+					$(".ball").addClass("bounce");
+				}
+
+				else {
+					$(".ball").removeClass("bounce");
+				}
+			
+			});
+		}
+
+		window.requestAnimationFrame(headerParallax);
+
+		$( ".example-wrap, example" ).hover(
+  			function() {
+    		$('.example-ball').addClass( "bouncing" );
+  			}
+		);
+
+	});
+});
